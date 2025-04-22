2af2d3afeb8d9c99a7bde63ad1ac33024af5eb91
muhammet <mavibirsaat@gmail.com>
2025-04-22 13:31:08 +0300

    simple form validation by manualley

--- AppBase/models.py
+++ AppBase/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
+
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField()
+    description = models.TextField()
--- AppBase/views.py
+++ AppBase/views.py
@@ -1,8 +1,43 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Article
 
 # Create your views here.
 
 
 def home(request):
-    return HttpResponse("hello worlds")
+    errors = {}
+    title = ""
+    description = ""
+
+    if request.method == "POST":
+        # retrieve data from form
+        title = request.POST.get("title", "").strip()
+        description = request.POST.get("description", "").strip()
+
+        # check for errors
+        if title == "":
+            errors["title"] = "bu alani doldurmak zorunludur"
+        elif len(title) < 4:
+            errors["title"] = "en az 4 harf olmali"
+        elif len(title) > 12:
+            errors["title"] = "en cok 12 harf olabilir"
+
+        if description == "":
+            errors["description"] = "bu alani doldurmak zorunludur"
+        elif len(description) < 4:
+            errors["description"] = "en az 4 harf olmali"
+        elif len(description) > 12:
+            errors["description"] = "en cok 12 harf olabilir"
+        # if no errors complete the form and save the Article
+        if not errors:
+            Article.objects.create(title=title, description=description)
+            return render(request, "success.html")
+
+    articles = Article.objects.all()
+    context = {
+        "errors": errors,
+        "title": title,
+        "description": description,
+        "articles": articles,
+    }
+    return render(request, "home.html", context=context)
