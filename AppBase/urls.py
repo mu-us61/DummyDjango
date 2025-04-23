b76e2d9e1653b888eebece32f853f40c95152a98
muhammet <mavibirsaat@gmail.com>
2025-04-23 16:50:04 +0300

    crud completed

--- AppBase/urls.py
+++ AppBase/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("create", views.create, name="create"),
-    path("read", views.read, name="read"),
-    path("update", views.update, name="update"),
-    path("delete", views.delete, name="delete"),
+    path("create/", views.create, name="create"),
+    path("read/<int:id>/", views.read, name="read"),
+    path("update/<int:id>/", views.update, name="update"),
+    path("delete/<int:id>", views.delete, name="delete"),
 ]
--- AppBase/views.py
+++ AppBase/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Article
 from .forms import ArticleForm
 from django.contrib import messages
@@ -6,6 +6,14 @@ from django.contrib import messages
 
 # Create your views here.
 def home(request):
+
+    articles = Article.objects.all()
+    context = {"articles": articles}
+
+    return render(request, "home.html", context=context)
+
+
+def create(request):
     if request.method == "POST":
         form = ArticleForm(request.POST)
         if form.is_valid():
@@ -15,23 +23,32 @@ def home(request):
     else:
         form = ArticleForm()
 
-    articles = Article.objects.all()
-    context = {"articles": articles, "form": form}
-
-    return render(request, "home.html", context=context)
+    context = {"form": form}
+    return render(request, "create.html", context=context)
 
 
-def create():
-    pass
+def read(request, id):
+    # article = Article.objects.get(id=id) #TODO sorulcak
+    article = get_object_or_404(Article, id=id)
+    return render(request, "read.html", {"article": article})
 
 
-def read():
-    pass
+def update(request, id):
+    article = get_object_or_404(Article, id=id)
+    if request.method == "POST":
+        form = ArticleForm(request.POST, instance=article)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "succesfully updated")
+            return redirect(read, id=id)
+    form = ArticleForm(instance=article)
+
+    return render(request, "update.html", {"form": form})
 
 
-def update():
-    pass
-
-
-def delete():
-    pass
+def delete(request, id):
+    article = get_object_or_404(Article, id=id)
+    if request.method == "POST":
+        article.delete()
+        return redirect("home")
+    return render(request, "delete.html", {"article": article})
