7cd94586ba57bce21ef30b2509be885ae12a2015
muhammet <mavibirsaat@gmail.com>
2025-04-23 10:55:43 +0300

    simple crud arrangements

--- AppBase/urls.py
+++ AppBase/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("create", views.create, name="create"),
+    path("read", views.read, name="read"),
+    path("update", views.update, name="update"),
+    path("delete", views.delete, name="delete"),
 ]
--- AppBase/views.py
+++ AppBase/views.py
@@ -3,8 +3,6 @@ from .models import Article
 from .forms import ArticleForm
 from django.contrib import messages
 
-# TODO form_asp acilacak daha ayrintili bir sekilde, kaynagi nerede nasil bakilir hepsi bakilcak
-
 
 # Create your views here.
 def home(request):
@@ -23,41 +21,17 @@ def home(request):
     return render(request, "home.html", context=context)
 
 
-# def home(request):
-#     errors = {}
-#     title = ""
-#     description = ""
+def create():
+    pass
 
-#     if request.method == "POST":
-#         # retrieve data from form
-#         title = request.POST.get("title", "").strip()
-#         description = request.POST.get("description", "").strip()
 
-#         # check for errors
-#         if title == "":
-#             errors["title"] = "bu alani doldurmak zorunludur"
-#         elif len(title) < 4:
-#             errors["title"] = "en az 4 harf olmali"
-#         elif len(title) > 12:
-#             errors["title"] = "en cok 12 harf olabilir"
+def read():
+    pass
 
-#         if description == "":
-#             errors["description"] = "bu alani doldurmak zorunludur"
-#         elif len(description) < 4:
-#             errors["description"] = "en az 4 harf olmali"
-#         elif len(description) > 12:
-#             errors["description"] = "en cok 12 harf olabilir"
-#         # if no errors complete the form and save the Article
-#         if not errors:
-#             Article.objects.create(title=title, description=description)
-#             messages.success(request, "the article created successfully")
-#             return redirect("home")
 
-#     articles = Article.objects.all()
-#     context = {
-#         "errors": errors,
-#         "title": title,
-#         "description": description,
-#         "articles": articles,
-#     }
-#     return render(request, "home.html", context=context)
+def update():
+    pass
+
+
+def delete():
+    pass
